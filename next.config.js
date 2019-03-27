5abfa01e961fe58212e5af13ca68dce5e22d6b67
Anthony Balmeo <anthonybalmeo@Anthonys-iMac.local>
2019-03-26 22:39:47 -0700

    Publish to github pages

--- next.config.js
+++ next.config.js
@@ -2,3 +2,9 @@ const withImages = require('next-images')
 const withFonts = require('next-fonts')
 
 module.exports = withImages(withFonts())
+
+module.exports = {
+  // some configuration
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/washabelle-lp' : '',
+  // another configuration
+}
